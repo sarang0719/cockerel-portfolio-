06149e577e781fb042fffa4f94925aec893fcf54
gsaran117 <saranbhuvana21@gmail.com>
2025-08-15 00:57:18 +0530

    its my creation of company portfolio

---
+++ script.js
@@ -0,0 +1,87 @@
+ $(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });   
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Developer", "Analyst", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed2 = new Typed(".typing-2", {
+        strings: ["Developer", "Analyst", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+
+    // blog filter script
+    $('.blog-filters button').click(function(){
+        $(this).addClass('active').siblings().removeClass('active');
+        var filter = $(this).text();
+
+        if(filter == 'All Posts'){
+            $('.blog-content .blog-card').show(400);
+        }else{
+            // hide all cards and then show the filtered ones
+            $('.blog-content .blog-card').hide(400);
+            $('.blog-content .blog-card[data-category="'+filter+'"]').show(400);
+        }
+    });
+});
